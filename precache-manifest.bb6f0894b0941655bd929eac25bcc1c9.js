b2511c9ccc88c0c6e57212800f02908138881946
Matin Sasanpour <matin.sasan@gmail.com>
2019-05-19 19:09:40 +0430

    Updates

---
+++ precache-manifest.bb6f0894b0941655bd929eac25bcc1c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0fe434f9e795b17c337b141e2dbf72",
+    "url": "/playground-react/index.html"
+  },
+  {
+    "revision": "544ac08cc2c39939a1d3",
+    "url": "/playground-react/static/css/main.7b67b5cd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/playground-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "544ac08cc2c39939a1d3",
+    "url": "/playground-react/static/js/main.9fcab5d3.chunk.js"
+  },
+  {
+    "revision": "0654c0353a5ee5632b6c",
+    "url": "/playground-react/static/js/runtime~main.e33c4099.js"
+  }
+]);
