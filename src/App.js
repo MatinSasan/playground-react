3eb3f5865a476898abae29230c691fa1176df301
Matin Sasanpour <matin.sasan@gmail.com>
2019-05-20 19:17:27 +0430

    error handling and css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
 import Person from './components/Person/Person';
+import ErrorBoundary from './ErrorBoundary/errorBoundary';
 
 import './App.css';
 
 class App extends Component {
   state = {
     persons: [
-      { id: null, name: 'Matin', age: 27 },
-      { id: null, name: 'Mobin', age: 26 }
+      { id: 1, name: 'Matin', age: 27 },
+      { id: 2, name: 'Mobin', age: 26 }
     ],
     VIP: { name: 'React' },
     showPersons: false,
-    mobin: true
+    mobin: true,
+    awesomePhrase: ''
   };
 
   nameHandler = newName => {
@@ -38,6 +40,18 @@ class App extends Component {
     const persons = [...this.state.persons].filter(
       person => person.id !== index
     );
+
+    if (persons.length === 0) {
+      this.setState({ awesomePhrase: 'where did they all go?' });
+    }
+
+    if (persons.some(p => p.name !== 'Mobin')) {
+      this.setState({ awesomePhrase: 'Where is Mobin?' });
+    }
+    if (persons.some(p => p.name !== 'Matin')) {
+      this.setState({ awesomePhrase: 'Where is Matin?' });
+    }
+
     this.setState({ persons });
   };
 
@@ -51,9 +65,11 @@ class App extends Component {
 
     const btn = {
       padding: '8px',
-      border: '1px solid #eee',
+      width: '100px',
+      norder: '1px solid #eee',
       cursor: 'pointer',
       backgroundColor: 'green',
+      border: '1px solid lightgreen',
       color: 'white'
     };
 
@@ -62,29 +78,55 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-          <button style={btn} onClick={this.nameHandler.bind(this, 'Mat')}>
+        <div className="person">
+          <br />
+          <button
+            disabled={this.state.persons.length === 0}
+            className="btn call"
+            onClick={this.nameHandler.bind(this, 'Mat')}>
             call me
           </button>
+          <br />
           <Person
             changed={this.inputHandler}
             name={this.state.VIP.name}
             mobin={this.state.mobin}>
             Hello there {':)'}
           </Person>
-          {this.state.persons.map((person, index) => {
+          {this.state.persons.map(person => {
             return (
-              <Person
-                key={(person.id = index)}
-                name={person.name}
-                age={person.age}
-                click={() => this.deleteHandler(index)}
-              />
+              <ErrorBoundary>
+                <Person
+                  key={person.id}
+                  name={person.name}
+                  age={person.age}
+                  click={() => this.deleteHandler(person.id)}
+                />
+              </ErrorBoundary>
             );
           })}
         </div>
       );
+      btn.backgroundColor = 'red';
+      btn.color = 'white';
     }
+
+    //
+    let awesomePhrase = '';
+    const classes = [];
+    if (!this.state.showPersons) {
+      awesomePhrase = 'Nobody is here, it seems :/';
+    }
+    if (this.state.showPersons && this.state.persons.length === 2) {
+      awesomePhrase = 'All aboard :D';
+    }
+    if (this.state.persons.length === 1) {
+      classes.push('bold');
+    }
+    if (this.state.persons.length === 0) {
+      classes.push('all');
+    }
+    console.log(this.state.persons.length);
 
     // Final render
     return (
@@ -92,6 +134,7 @@ class App extends Component {
         <button style={btn} onClick={() => this.switchHandler()}>
           Toggle
         </button>
+        <h2 className={classes}>{this.state.awesomePhrase || awesomePhrase}</h2>
         {persons}
       </div>
     );
